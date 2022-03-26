aca746e25f5d6d4ef00db5b7cfb1a05658576e20
Zachary Spriggs <zspriggs14@gmail.com>
2022-03-26 18:52:48 -0400

    Delete taskdlg.h

--- taskdlg.h
+++
@@ -1,9 +0,0 @@
-ptrszint FUNC_TASKDLGCALLBACK(ptrszint*_FUNC_TASKDLGCALLBACK_OFFSET_HWND,uint32*_FUNC_TASKDLGCALLBACK_ULONG_MSG,uptrszint*_FUNC_TASKDLGCALLBACK_UOFFSET_WPARAM,ptrszint*_FUNC_TASKDLGCALLBACK_OFFSET_LPARAM,ptrszint*_FUNC_TASKDLGCALLBACK_OFFSET_LPREFDATA);
-
-HRESULT TaskDialogCallback(HWND hwnd, UINT Msg, WPARAM wParam, LPARAM lParam, LONG_PTR lpRefData){
-	return FUNC_TASKDLGCALLBACK((ptrszint *) (& hwnd), & Msg, & wParam, (ptrszint *) (& lParam), &lpRefData);
-}
-
-void * TaskDialogCallbackProc(){
-	return (void *) TaskDialogCallback;
-}
